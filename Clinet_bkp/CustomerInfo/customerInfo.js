a62ed19f4b71c660699ea60b4a342dac58bebaa6
varun9797 <verma9797@gmail.com>
2017-10-22 14:26:14 -0400

    deleting unnesesary file

--- Clinet_bkp/CustomerInfo/customerInfo.js
+++
@@ -1,7 +0,0 @@
-var app= app.controller("customerController", ['$scope',function($scope) { 
-    $scope.address = "A-5 Sanjeevni Society pune"  
-    $scope.name = "vik verma";
-    $scope.age = "27";
-    $scope.pno = "297467282";
-}]);
-    
--- Clinet_bkp/ProductInfo/productInfo.js
+++
@@ -1,9 +0,0 @@
-var app= app.controller("productInfoController", ['$scope',function($scope) { 
-    $scope.address = "A-5 Sanjeevni Society pune";    
-    
-    $scope.$on("myFirstBraodcast", function(b, data){
-        console.log("listining broadcasr in product info");
-    })
-    
-}]);
-    
